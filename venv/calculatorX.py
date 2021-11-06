fd4e428315bf01e11b968e53ceca328ad5382d0e
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-11-06 17:15:09 -0400

    Change 4

---
+++ venv/calculatorX.py
@@ -0,0 +1,40 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.lang.builder import Builder
+
+Builder.load_file('./calculatorX.kv')
+Window.size = (350, 550)
+
+
+class CalculatorWidget(Widget):
+    def clear(self):
+        self.ids.input_box.text = '0'
+
+    def button_value(self, number):
+        prev_number = self.ids.input_box.text
+
+        if prev_number == '0':
+            self.ids.input_box.text = ''
+            self.ids.input_box.text = f"{number}"
+        else:
+            self.ids.input_box.text = f"{prev_number}{number}"
+
+    def signs(self, sign):
+        prev_number = self.ids.input_box.text
+        self.ids.input_box.text = f"{prev_number}{sign}"
+
+    def remove_last(self):
+        prev_number = self.ids.input_box.text
+        prev_number = prev_number[:-1]
+        self.ids.input_box.text = prev_number
+
+
+class CalculatorApp(App):
+    def build(self):
+        return CalculatorWidget()
+
+
+if __name__ == "__main__":
+    CalculatorApp().run()
+
